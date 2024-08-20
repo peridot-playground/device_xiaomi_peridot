18784d28a52d1100e82396a3ce211dfbe55891d6
ahmedtohamy1 <ahmedtohamy1@users.noreply.github.com>
2024-08-20 15:00:15 +0000

    peridot: Compile HWUI for performance * Activates a few optimizing compiler flags for HWUI. * -fno-omit-frame-pointer, -marm, -mapcs
    Signed-off-by: Hikari-Soni <247soni01@gmail.com>
    Signed-off-by: Drenzzz <realdrenzzz@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -76,6 +76,9 @@ ODM_MANIFEST_FILES := $(DEVICE_PATH)/configs/hidl/manifest_odm.xml
 # Inherit from proprietary files for miuicamera
 -include device/xiaomi/peridot-miuicamera/BoardConfig.mk
 
+# HWUI
+HWUI_COMPILE_FOR_PERF := true
+
 # Kernel
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 BOARD_RAMDISK_USE_LZ4 := true
