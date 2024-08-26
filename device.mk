4da46c57a307fa97aef4023d6e86853b9f0029fb
Michael Bestas <mkbestas@lineageos.org>
2024-08-27 02:04:54 +0300

    peridot: Work around using prebuilt HWC
    After enabling ELF checks and switching prebuilts to modules,
    the init and vintf files are no longer automatically built.
    
    Change-Id: I48fcab53a7c8128b18e1399d9afadcfd5c85b116
    Signed-off-by: Drenzzz <realdrenzzz@gmail.com>

--- device.mk
+++ device.mk
@@ -219,7 +219,9 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer3-V2-ndk.vendor \
     android.hardware.graphics.composer@2.1.vendor \
     android.hardware.graphics.composer@2.2.vendor \
-    android.hardware.graphics.composer@2.3.vendor
+    android.hardware.graphics.composer@2.3.vendor \
+    vendor.qti.hardware.display.composer-service.rc \
+    vendor.qti.hardware.display.composer-service.xml
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.compute-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.compute-0.xml \
