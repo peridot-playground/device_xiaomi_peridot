493f5993ef1f46aa8e751ba60d289d8f415d9564
Guidix <guidixm@gmail.com>
2025-02-08 15:49:24 +0200

    peridot: Switch to AIDL UDFPS Fingerprint HAL
    - As of Android 15, AIDL Fingerprint HAL is required to set fingerprint icon indicator pos.
    - Also set UDFPS Sensor position
    - Drop mfp-daemon since we no loger using xiaomi prebuilt fp hal
    - Label sepolicy denials related to fingerprint
    - Setup Rootdir permissions for goodix fingerprint
    Signed-off-by: GuidixX <guidixm@gmail.com>
    
    Signed-off-by: Drenzzz <realdrenzzz@gmail.com>

--- device.mk
+++ device.mk
@@ -207,9 +207,7 @@ PRODUCT_COPY_FILES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.common.thread.vendor \
-    android.hardware.biometrics.common.util.vendor \
-    android.hardware.biometrics.fingerprint-V3-ndk.vendor
+    android.hardware.biometrics.fingerprint-service.xiaomi
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
@@ -286,7 +284,8 @@ PRODUCT_PACKAGES += \
     init.qcom.rc \
     init.qti.kernel.rc \
     init.recovery.qcom.rc \
-    init.target.rc
+    init.target.rc \
+    init.fingerprint.rc
 
 PRODUCT_PACKAGES += \
     ueventd-odm.rc \
