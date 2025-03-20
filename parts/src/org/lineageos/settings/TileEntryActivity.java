af72775b1ec2a2f4c3fc07444dcbf26ca5a5b849
Guidix <guidixm@gmail.com>
2025-03-20 03:26:02 +0200

    peridot: parts: Add long press action for GameBar Overlay
    Signed-off-by: GuidixX <guidixm@gmail.com>
    
    q
    
    Signed-off-by: Drenzzz <realdrenzzz@gmail.com>

--- parts/src/org/lineageos/settings/TileEntryActivity.java
+++ parts/src/org/lineageos/settings/TileEntryActivity.java
@@ -30,6 +30,7 @@ import org.lineageos.settings.refreshrate.RefreshActivity;
 import org.lineageos.settings.touchsampling.TouchSamplingSettingsActivity;
 import org.lineageos.settings.thermal.ThermalSettingsActivity;
 import org.lineageos.settings.saturation.SaturationActivity;
+import org.lineageos.settings.gamebar.GameBarSettingsActivity;
 
 public class TileEntryActivity extends Activity {
     private static final String TAG = "TileEntryActivity";
@@ -37,6 +38,7 @@ public class TileEntryActivity extends Activity {
     private static final String HTSR_TILE = "org.lineageos.settings.touchsampling.TouchSamplingTileService";
     private static final String THERMAL_TILE = "org.lineageos.settings.thermal.ThermalTileService";
     private static final String SATURATION_TILE = "org.lineageos.settings.saturation.SaturationTileService";
+    private static final String GAMEBAR_TILE = "org.lineageos.settings.gamebar.GameBarTileService";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -59,6 +61,8 @@ public class TileEntryActivity extends Activity {
             intent = new Intent(this, ThermalSettingsActivity.class);
         } else if (SATURATION_TILE.equals(sourceClassName)) {
             intent = new Intent(this, SaturationActivity.class);
+        } else if (GAMEBAR_TILE.equals(sourceClassName)) {
+            intent = new Intent(this, GameBarSettingsActivity.class);
         } else {
             Log.e(TAG, "Unknown tile: " + sourceClassName);
             finish();
