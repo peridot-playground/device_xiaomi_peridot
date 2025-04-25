1cfa97b60bae3375876f218692d4cf01e48fe668
kenway214 <kenway214@outlook.com>
2025-04-26 01:09:36 +0530

    peridot: parts: TurboCharging: Refactor and add 18W charging option
    - Add new 18W option to Turbo Charging wattage list
    - Set default Turbo Charging and Sports Mode states to ON
    - Automatically disable Sports Mode when Turbo Charging is turned off
    - Force-disable Sports Mode when 18W charging is selected
    - Fix QS tile not updating on system UI start or state change
    - Minor cleanups and formatting improvements
    
    Signed-off-by: kenway214 <kenway214@outlook.com>
    Signed-off-by: Drenzzz <realdrenzzz@gmail.com>

--- parts/src/org/lineageos/settings/turbocharging/TurboChargingActivity.java
+++ parts/src/org/lineageos/settings/turbocharging/TurboChargingActivity.java
@@ -17,19 +17,21 @@
 package org.lineageos.settings.turbocharging;
 
 import android.os.Bundle;
+import androidx.preference.PreferenceManager;
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
 
 import org.lineageos.settings.R;
 
 public class TurboChargingActivity extends CollapsingToolbarBaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        PreferenceManager.setDefaultValues(this, R.xml.turbocharging, false);
 
-     @Override
-     protected void onCreate(Bundle savedInstanceState) {
-         super.onCreate(savedInstanceState);
-         setContentView(R.layout.turbocharging_layout);
-         getFragmentManager()
-                 .beginTransaction()
-                 .replace(R.id.content_frame, new TurboChargingFragment())
-                 .commit();
-     }
- }
+        setContentView(R.layout.turbocharging_layout);
+        getFragmentManager()
+                .beginTransaction()
+                .replace(R.id.content_frame, new TurboChargingFragment())
+                .commit();
+    }
+}
